c8f33671350afd9dc96b36e49680b2873785d01c
fghuytgy7 <gs1imran.ku@hotmail.com>
2024-05-15 09:53:29 +0700

    test project

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const word = "hello";
-const reversedWord = word.split("").reverse().join("");
-console.log(reversedWord); // olleh
+function lowestCommonAncestor(root, p, q) {
+  if (!root || root === p || root === q) return root;
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
+  if (left && right) return root;
+  return left ? left : right;
+}
